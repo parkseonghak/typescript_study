c444a98880a18c466aee1ab43fe253fc5ba40d15
parkseonghak <100550415+parkseonghak@users.noreply.github.com>
2022-10-03 18:07:27 +0900

    강의04

---
+++ practice/function_type/function_type.ts
@@ -0,0 +1,20 @@
+const waitingBtn: any = document.querySelector('.human_name');
+const listBtn: any = document.querySelector('.human_list');
+const joinBtn: any = document.querySelector('.human_join');
+const customerList: Array<string> = []
+
+waitingBtn.addEventListener('click', function():{
+    customerList.push(prompt("이름을 입력하세요."));
+})
+
+listBtn.addEventListener('click', viewList);
+
+function viewList(){
+    alert('현재대기자는 ' + customerList + "이며 총 " + customerList.length + "명 입니다.");
+}
+
+joinBtn.addEventListener('click', humanJoin)
+
+function humanJoin(){
+    alert(customerList.shift() + "님 입장합니다.");
+}
